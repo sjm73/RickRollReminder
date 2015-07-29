924b6b830bc4ed7bd83c557b712705f21258a9c1
Steve Matthews <steve@usertesting.com>
2015-07-29 12:52:19 -0700

    Initial addition of xcode project

---
+++ RickRollReminder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RickRollReminder
+//
+//  Created by Steve Matthews on 7/29/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
